9ab690fddcbfad4a2b7eb0cc2856e49b04a9c88f
JoseManuelGonzalezHernandez <78152048+JoseManuelGonzalezHernandez@users.noreply.github.com>
2021-09-23 17:34:31 +0100

    Feature: Añadida la funcionalidad para obtener un nombre aleatorio del archivo CSV

--- src/Listado.java
+++ src/Listado.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Listado {
     public static void main(String[] args) {
@@ -10,12 +11,29 @@ public class Listado {
             archivo = new File("C:\\Users\\joseg\\Desktop\\Carpeta Compartida W10\\D.A.M\\2º\\Goyo\\Programacion multimedia y dispositivos moviles\\Practica1-1\\src\\ListadoDeNombres.csv");
             fr = new FileReader(archivo);
             br = new BufferedReader(fr);
+            Scanner s = null;
 
-            // Lectura del fichero
             String linea;
-            while ((linea = br.readLine()) != null)
-                System.out.println(linea);
+            double registro = 0;
+            int count = 0;
 
+            s = new Scanner(archivo);
+            while (s.hasNextLine()) {
+                linea = s.nextLine();
+                registro++;
+            }
+
+            double registroRandom = Math.floor(Math.random()*(registro-2+2)+2);
+
+            while ((linea = br.readLine()) != null) {
+                //System.out.println("Registro random: " + registroRandom);
+                //System.out.println("Contador: " + count);
+                count++;
+                if (count == registroRandom) {
+                    System.out.println(linea);
+                    break;
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
