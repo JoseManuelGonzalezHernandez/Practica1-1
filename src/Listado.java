a30970933d42ddd56fba4d5c0e96cdcf89776ab4
JoseManuelGonzalezHernandez <78152048+JoseManuelGonzalezHernandez@users.noreply.github.com>
2021-09-28 11:35:17 +0100

    Feature: Cambiada la forma de realizar el problema guardando los datos en un array

--- src/Listado.java
+++ src/Listado.java
@@ -8,48 +8,64 @@ public class Listado {
         BufferedReader br = null;
 
         try {
+            //CARGA DE ARCHIVO
             archivo = new File("C:\\Users\\joseg\\Desktop\\Carpeta Compartida W10\\D.A.M\\2º\\Goyo\\Programacion multimedia y dispositivos moviles\\Practica1-1\\src\\ListadoDeNombres.csv");
             fr = new FileReader(archivo);
             br = new BufferedReader(fr);
             Scanner s = null;
 
-            String linea;
-            double registro = 0;
-            int count = 0;
+            //ARRAYS DE NOMBRES
+            String[] names;
 
+            //VARIABLES GLOBALES
+            String linea;
+            int registro = 0;
+            int count = 0;
+            int personas = 10;
+
+            //ADIVINAR REGISTROS EN EL ARCHIVO
             s = new Scanner(archivo);
             while (s.hasNextLine()) {
-                linea = s.nextLine();
+                s.nextLine();
                 registro++;
             }
+            //INICIALIZAR EL ARRAYS CON LOS REGISTROS CALCULADOS ANTERIORMENTE
+            names = new String[registro];
+            System.out.println(names.length);
 
-            double registroRandom = Math.floor(Math.random()*(registro-2+1)+2);
-
+            //INTRODUCIR EN EL ARRAYS LOS DATOS DEL ARCHIVO
             while ((linea = br.readLine()) != null) {
-                //System.out.println("Registro random: " + registroRandom);
-                //System.out.println("Contador: " + count);
-                count++;
-                if (count == registroRandom) {
-                    double nota1 = truncXDecimales((Math.random()*10),2);
-                    double nota2 = truncXDecimales((Math.random()*10),2);
-                    double nota3 = truncXDecimales((Math.random()*10),2);
-                    double nota4 = truncXDecimales((Math.random()*10),2);
-                    //Formula redondear X decimales -> Math.round(NUMERO*Math.pow(10,X)/Math.pow(10.X);
-                    double notaMedia = truncXDecimales(((nota1 + nota2 + nota3 + nota4)/4),2);
-
-
-                    System.out.println(linea);
-                    System.out.println("Nota 1: " + nota1);
-                    System.out.println("Nota 2: " + nota2);
-                    System.out.println("Nota 3: " + nota3);
-                    System.out.println("Nota 4: " + nota4);
-                    System.out.println("Nota Media: " + notaMedia);
-                    break;
+                if (count < registro) {
+                    names[count] = linea;
+                    count++;
                 }
             }
+
+            //MOSTRAR 10 PERSONAS ALEATORIAS Y 4 NOTAS ALEATORIAS
+            for (int i = 0; i < personas; i++) {
+                //ELEGIR UNA POSICION ALEATORIA DEL ARRAY
+                int posRandom = (int) Math.floor(Math.random()*((registro-1)-1+1)+1);
+
+                //CALCULAR NOTAS RANDOM -> Formula redondear X decimales -> Math.round(NUMERO*Math.pow(10,X)/Math.pow(10.X);
+                double nota1 = truncXDecimales((Math.random()*10),2);
+                double nota2 = truncXDecimales((Math.random()*10),2);
+                double nota3 = truncXDecimales((Math.random()*10),2);
+                double nota4 = truncXDecimales((Math.random()*10),2);
+                double notaMedia = truncXDecimales(((nota1 + nota2 + nota3 + nota4)/4),2);
+                //MOSTRAR TODA LA INFORMACIÓN
+                System.out.println(names[posRandom]);
+                System.out.println("Nota 1: " + nota1);
+                System.out.println("Nota 2: " + nota2);
+                System.out.println("Nota 3: " + nota3);
+                System.out.println("Nota 4: " + nota4);
+                System.out.println("Nota Media: " + notaMedia);
+                System.out.println("****************");
+            }
         } catch (Exception e) {
+            //MOSTRAR ERROR
             e.printStackTrace();
         } finally {
+            //CIERRE
             try {
                 if (null != br) {
                     br.close();
@@ -58,11 +74,13 @@ public class Listado {
                     fr.close();
                 }
             } catch (Exception e2) {
+                //MOSTRAR ERROR
                 e2.printStackTrace();
             }
         }
     }
 
+    //FUNCION PARA TRUNCAR SEGUN LOS DECIMALES QUE LE PASEMOS
     public static double truncXDecimales(double num,int decimales) {
         return Math.round(num*Math.pow(10,decimales))/Math.pow(10,decimales);
     }
